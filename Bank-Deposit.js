db2c348bce20d6dd37382994f1d0fdf4d9ecad8a
mdmahbubur007 <61216155+mdmahbubur007@users.noreply.github.com>
2020-02-29 16:12:58 +0600

    Add Balance

--- Bank-Deposit.js
+++ Bank-Deposit.js
@@ -7,14 +7,34 @@ log_in_button.addEventListener("click", function(){
     transactionArea.style.display = "block";
 })
 
+// //////////////// deposit button event handler ///////////////////
+// const depositBtn = document.getElementById("addDeposit");
+// depositBtn.addEventListener("click", function(){
+//     const depositedAmount = document.getElementById("depositAmount").value;
+//     const depositNumber=parseFloat(depositedAmount)||0;
+
+//     const currentDeposit = document.getElementById("currentDeposit").innerText;
+//     const currentDepositedAmount=parseFloat("currentDeposit");
+//     const totalDeposit=depositNumber+currentDepositedAmount;
+//     document.getElementById("currentDeposit").innerText=totalDeposit;
+// })
+
+
 //////////////// deposit button event handler ///////////////////
 const depositBtn = document.getElementById("addDeposit");
 depositBtn.addEventListener("click", function(){
     const depositedAmount = document.getElementById("depositAmount").value;
-    const depositNumber=parseFloat(depositedAmount);
-
+    const depositNumber=parseFloat(depositedAmount) || 0;
+ 
     const currentDeposit = document.getElementById("currentDeposit").innerText;
-    const currentDepositedAmount=parseFloat("currentDeposit");
+    const currentDepositedAmount=parseFloat(currentDeposit);
     const totalDeposit=depositNumber+currentDepositedAmount;
     document.getElementById("currentDeposit").innerText=totalDeposit;
+
+    const currentBalance = document.getElementById("currentBalance").innerText;
+    const currentBalanceAmount=parseFloat("currentBalance");
+    const totalBalance=currentBalanceAmount+depositNumber;
+    document.getElementById("currentBalance").innerText=totalBalance;
+
+    document.getElementById("depositAmount").value =" ";
 })
