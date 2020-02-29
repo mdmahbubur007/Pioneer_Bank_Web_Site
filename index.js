dec4e14997980ff7e5f17b68cd06f9403a06c876
mdmahbubur007 <61216155+mdmahbubur007@users.noreply.github.com>
2020-02-29 19:07:17 +0600

    amount added

---
+++ index.js
@@ -0,0 +1,29 @@
+////////////// log in button event handler////////////
+const log_in_button = document.getElementById("login");
+log_in_button.addEventListener("click", function(){
+    const loginArea = document.getElementById("log-in-area")
+    loginArea.style.display="none";
+    const transactionArea = document.getElementById("transaction-Area");
+    transactionArea.style.display = "block";
+})
+
+//////////////// deposit button event handler ///////////////////
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function(){
+    const depositedAmount = document.getElementById("depositAmount").value;
+    const depositNumber=parseFloat(depositedAmount) || 0;
+
+    const currentDeposit = document.getElementById("currentDeposit").innerText;
+    const currentDepositedAmount=parseFloat(currentDeposit);
+    const totalDeposit=depositNumber+currentDepositedAmount;
+    document.getElementById("currentDeposit").innerText=totalDeposit;
+
+    // total balance
+  const mainBalacne  = document.getElementById("main-balance").innerText;
+  const updateBalnce = parseFloat(mainBalacne) + depositNumber;
+
+  // update text current balance
+  document.getElementById("main-balance").innerText = updateBalnce;
+
+  document.getElementById("depositAmount").value = "";
+})
